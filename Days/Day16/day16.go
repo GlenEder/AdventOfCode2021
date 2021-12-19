daf007bef798edf6140b8266426fffd5572db6b5
Glen Eder <glen.ma.eder@gmail.com>
2021-12-19 10:00:29 -0500

    Adds command line args to run specific days'

---
+++ Days/Day16/day16.go
@@ -0,0 +1,13 @@
+package Day16
+
+import (
+	"src/Utils"
+)
+
+
+func Run() {
+	Utils.PrintDay(16)
+
+	//get input
+	//input := Utils.ReadInputToSlice("Days/Day16/input.txt")
+}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	"reflect"
 	"src/Days/Day1"
 	"src/Days/Day10"
 	"src/Days/Day11"
@@ -8,6 +10,7 @@ import (
 	"src/Days/Day13"
 	"src/Days/Day14"
 	"src/Days/Day15"
+	"src/Days/Day16"
 	"src/Days/Day2"
 	"src/Days/Day3"
 	"src/Days/Day4"
@@ -21,21 +24,37 @@ import (
 )
 
 func main() {
+
+	//Retrieve args
+	daysToRun := os.Args[1:]
+	runAll := true
+	if len(daysToRun) > 0 { runAll = false }
+
+		days := []interface{}{
+		Day1.Run,
+		Day2.Run,
+		Day3.Run,
+		Day4.Run,
+		Day5.Run,
+		Day6.Run,
+		Day7.Run,
+		Day8.Run,
+		Day9.Run,
+		Day10.Run,
+		Day11.Run,
+		Day12.Run,
+		Day13.Run,
+		Day14.Run,
+		Day15.Run,
+		Day16.Run,
+	}
+
 	now := time.Now()
-	Day1.Run()
-	Day2.Run()
-	Day3.Run()
-	Day4.Run()
-	Day5.Run()
-	Day6.Run()
-	Day7.Run()
-	Day8.Run()
-	Day9.Run()
-	Day10.Run()
-	Day11.Run()
-	Day12.Run()
-	Day13.Run()
-	Day14.Run()
-	Day15.Run()
+	for i, day := range days {
+		if runAll || Utils.SliceContainsString(daysToRun, Utils.IntToString(i + 1)) {
+			method := reflect.ValueOf(day)
+			method.Call(nil)
+		}
+	}
 	Utils.PrintWithColor(Utils.Yellow, "\nTime to compute: " + time.Since(now).String() + "\n")
 }
